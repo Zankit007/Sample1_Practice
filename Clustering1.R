eccdbb96fb66a86407c8c613dfb33fae81fd780e
Ankit Zade <ankitzade007@gmail.com>
2020-07-26 22:15:13 +0530

    HI - added

--- Clustering1.R
+++ Clustering1.R
@@ -9,4 +9,7 @@ model1 = kmeans(subset(A,select = -c(Species)),centers = 2,nstart = 20)
 model1$cluster
 A$clusters = as.factor(model1$cluster)
 table(A$clusters,A$Species)   ###
+
+
 ##Trying to Commit
+## Hi
